10c898e663532ca50ad56d94f917a6a865fe163f
Iván Tortosa <152330066+LiquidRay-1@users.noreply.github.com>
2024-03-11 20:17:04 +0100

    Add files via upload

---
+++ ListadeTareas/ListadeTareas/ListadeTareasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListadeTareasApp.swift
+//  ListadeTareas
+//
+//  Created by dam2 on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ListadeTareasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
